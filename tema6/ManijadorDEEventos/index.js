efa585c4511e0c188340917f7aba40db6fb11d8b
Nouhaila2023 <nelh237@g.educaand.es>
2026-01-23 21:50:32 +0100

    d

--- tema6/ManijadorDEEventos/index.js
+++ tema6/ManijadorDEEventos/index.js
@@ -1,8 +1,10 @@
 window.onload = iniciar;
 
 function iniciar() {
-  document.getElementById("btnEnviar").addEventListener("click", manejador);
   document.querySelector("p").addEventListener("click", manejador);
+    document.querySelector("p").addEventListener("mouseover", manejador);
+     document.querySelector("li").addEventListener("click", manejador);
+
 }
 
 function manejador(e) {
@@ -24,6 +26,20 @@ function manejador(e) {
         alert("Has pulsado el boton enviar");
         let parrafo = document.querySelectorAll("p").length;
         alert("El numero de parrafos es: " + parrafo);
+    }
+
+    if(e.target.closest("li")){
+        switch (e.type){
+            case "click":
+               let linuevo=document.createElement("li");
+               linuevo.textContent = prompt("Entredoce Elemnto de la lista");
+               e.target.closest("li").appendChild(linuevo);
+            
+            case "mouseover":
+               /* e.target.closest("p").style.backgroundColor = "yellow";
+                e.target.closest("p").style.cursor = "pointer";*/
+                break;    
+        }
     }
 
 }
